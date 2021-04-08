8d22eb475a0949880f61c8a771c4f35040f33be5
Rafael Henrique Lisot <rafael.lisot@unoesc.edu.br>
2021-04-07 21:30:03 -0300

    criando rota de teste e configurando JWTAuth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/ping', function() {
+    return ['pong'=> true];
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
