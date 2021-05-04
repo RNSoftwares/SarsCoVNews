145b751ba89be1e6f971e153e0626cba034e29da
Natan Trennepohl <natan.gabriel@unoesc.edu.br>
2021-05-04 15:53:37 -0300

    tela de login pronta

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -4,15 +4,29 @@ import './style.css';
 function login() {
 
     return(
-        <div className="login">
+        <div className="body">
             <div className="titulo">
-
+                <span>SarsCovNews</span>
             </div>
-            
-            <div className="conteudo">
-
+            <div className="corpo">
+                <div className="login">
+                    <div className="email">
+                        <div className="icone">
+                            <img src="./images/login.svg"/>
+                        </div>
+                        <input type="text" placeholder="E-mail"></input>
+                    </div>
+                    <div className="senha">
+                        <div className="icone">
+                            <img src="./images/padlock.svg"/>
+                        </div>
+                        <input type="password" placeholder="Senha"></input>
+                    </div>
+                    <div className="botao">
+                        <button type="submit">Entrar</button>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
