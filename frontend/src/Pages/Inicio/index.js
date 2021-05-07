de1667135a74385c5a7798d2073f7851a7646e4a
Natan Trennepohl <natan.gabriel@unoesc.edu.br>
2021-05-07 15:55:23 -0300

    atts

--- frontend/src/Pages/Inicio/index.js
+++ frontend/src/Pages/Inicio/index.js
@@ -5,7 +5,16 @@ import './style.css';
 function inicio(){
     return(
         <div className="corpoInicio">
-            
+            <div className="title">
+                    <span>SarsCovNews</span>
+                    <p className="texto">Sistema de Monitoramento de casos de COVID-19</p>
+            </div>
+            <div className="dados">
+                    ...
+            </div>
+            <div className="estats">
+                    ...
+            </div>
         </div>
     )
 
