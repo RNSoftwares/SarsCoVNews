8cfb411ebc9be955c0756decf566507b433d006d
Rafael Henrique Lisot <rafael.lisot@unoesc.edu.br>
2021-05-19 19:13:35 -0300

    Corrigindo back e front

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,2 +1,37 @@
 const baseURL = 'http://127.0.0.1:8000/api';
 
+const request = async (method, endpoint, params, token = null) => {
+    method = method.toLowerCase();
+    let fullUrl = `${baseUrl}${endpoint}`;
+    let body = null;
+
+    switch (method) {
+        case 'get':
+            let queryString = new URLSearchParams(params).toString();
+            fullUrl += `?${queryString}`;
+        break;
+
+        case 'post':
+        case 'put':
+        case 'delete':
+            body = JSON.stringify(params);
+        break;
+    }
+
+    let headers = {'Content-type': 'application/json'};
+
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    let req = await fetch(fullUrl, {method, headers, body});
+    let json = await req.json();
+    
+    return json;
+}
+
+export default {
+    register: async () => {
+        
+    }
+};
